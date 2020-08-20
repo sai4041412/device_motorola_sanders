e1b2ba79aa672f3eb0f87ece42ddc6d34921485f
ashwatthama <sai404142@gmail.com>
2020-08-20 16:26:01 +0000

    RR Bringup
    Signed-off-by: ashwatthama <sai404142@gmail.com>

--- rr_sanders.mk
+++ rr_sanders.mk
@@ -22,8 +22,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk
 # Inherit from sanders device
 $(call inherit-product, device/motorola/sanders/device.mk)
 
-# Inherit some cmmon xtended stuff.
-$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
+# Inherit some cmmon rr stuff.
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
 # Boot animation
 TARGET_SCREEN_WIDTH := 1080
@@ -32,7 +32,7 @@ TARGET_BOOT_ANIMATION_RES := 1080
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := sanders
-PRODUCT_NAME := xtended_sanders
+PRODUCT_NAME := rr_sanders
 PRODUCT_BRAND := motorola
 PRODUCT_MANUFACTURER := motorola
 
@@ -49,3 +49,6 @@ VENDOR_BUILD_FINGERPRINT := google/walleye/walleye:8.1.0/OPM1.171019.011/4448085
 
 # for specific
 $(call inherit-product, vendor/motorola/sanders/sanders-vendor.mk)
+
+# Resurrection Remix
+TARGET_FACE_UNLOCK_SUPPORTED := true
