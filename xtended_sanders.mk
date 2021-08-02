6b1b2aae25be57885a0a83406d84327570bb7172
ashwatthama <sai404142@gmail.com>
2021-08-02 07:12:23 +0000

    Xtended go official

--- xtended_sanders.mk
+++ xtended_sanders.mk
@@ -30,6 +30,12 @@ TARGET_SCREEN_WIDTH := 1080
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_BOOT_ANIMATION_RES := 1080
 
+# Official MSM-Xtended
+XTENDED_BUILD_TYPE := OFFICIAL
+
+#Maintainer
+XTENDED_BUILD_MAINTAINER := Ashwatthama
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := sanders
 PRODUCT_NAME := xtended_sanders
